648fb5a14ff6c263947d84b9d1318492b05359d4
Dionisio, Farrah <farrah.dionisio@accenture.com>
2021-10-28 15:12:20 +0800

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
+      obcaecati consectetur alias praesentium vitae aspernatur, exercitationem
+      expedita tempora doloribus, quod aperiam nisi dolore voluptas corporis.
+      Reiciendis eligendi perferendis ratione ducimus sit nisi eveniet quod
+      obcaecati corrupti, voluptates ipsam facilis autem nemo. Nostrum ea
+      incidunt earum quidem? Eligendi temporibus, sed blanditiis fugiat
+      molestias corporis sapiente iusto itaque dicta corrupti perspiciatis
+      pariatur consequuntur distinctio inventore culpa vel numquam dolore,
+      mollitia excepturi quas autem. Odio dolore illum asperiores aspernatur
+      ullam aut! Laboriosam, enim magnam dignissimos provident adipisci ipsa
+      dicta. Cum eveniet vitae obcaecati ex quos, corporis recusandae
+      repudiandae tempora non debitis. Omnis, aut.
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
+      obcaecati consectetur alias praesentium vitae aspernatur, exercitationem
+      expedita tempora doloribus, quod aperiam nisi dolore voluptas corporis.
+      Reiciendis eligendi perferendis ratione ducimus sit nisi eveniet quod
+      obcaecati corrupti, voluptates ipsam facilis autem nemo. Nostrum ea
+      incidunt earum quidem? Eligendi temporibus, sed blanditiis fugiat
+      molestias corporis sapiente iusto itaque dicta corrupti perspiciatis
+      pariatur consequuntur distinctio inventore culpa vel numquam dolore,
+      mollitia excepturi quas autem. Odio dolore illum asperiores aspernatur
+      ullam aut! Laboriosam, enim magnam dignissimos provident adipisci ipsa
+      dicta. Cum eveniet vitae obcaecati ex quos, corporis recusandae
+      repudiandae tempora non debitis. Omnis, aut.
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, et sint.
+      Placeat sed reiciendis unde architecto rem porro repellendus, delectus,
+      natus, modi dignissimos corrupti perspiciatis vero quaerat nemo iste eum
+      ut voluptatibus earum ipsum. Hic sequi et aspernatur modi vero laboriosam,
+      architecto repellat nulla nobis saepe excepturi, aut repudiandae a
+      quibusdam expedita tempore eveniet quam velit distinctio molestias omnis
+      perspiciatis. Repudiandae ipsum porro, eveniet fugiat optio qui odit
+      excepturi nostrum perferendis voluptas hic inventore perspiciatis cumque
+      adipisci. Alias debitis ex hic ipsum sint rem facilis nisi voluptatem.
+      Ipsam adipisci praesentium autem tempore, sunt rem maxime, blanditiis
+      nostrum iste inventore exercitationem.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, et sint.
+      Placeat sed reiciendis unde architecto rem porro repellendus, delectus,
+      natus, modi dignissimos corrupti perspiciatis vero quaerat nemo iste eum
+      ut voluptatibus earum ipsum. Hic sequi et aspernatur modi vero laboriosam,
+      architecto repellat nulla nobis saepe excepturi, aut repudiandae a
+      quibusdam expedita tempore eveniet quam velit distinctio molestias omnis
+      perspiciatis. Repudiandae ipsum porro, eveniet fugiat optio qui odit
+      excepturi nostrum perferendis voluptas hic inventore perspiciatis cumque
+      adipisci. Alias debitis ex hic ipsum sint rem facilis nisi voluptatem.
+      Ipsam adipisci praesentium autem tempore, sunt rem maxime, blanditiis
+      nostrum iste inventore exercitationem.
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
